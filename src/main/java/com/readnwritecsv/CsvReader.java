4333d4198d41da802542027af03183f4e5d3b6f2
atarikkilic <tarikkilic07@gmail.com>
2023-03-14 13:15:58 +0300

    CsvReader class changes
    A class responsible for reading data from the CSV file
    
    1. Import input-data.csv
    
    2. Read the file

--- src/main/java/com/readnwritecsv/CsvReader.java
+++ src/main/java/com/readnwritecsv/CsvReader.java
@@ -1,5 +1,40 @@
 package com.readnwritecsv;
 
-public class CsvReader {
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+
+public class CsvReader {
+	
+	//Store the data from the CSV file as a list of string arrays
+	public static List<String[]> r;
+	
+	//That reads the data from a CSV file
+	public void readerCsv() throws IOException, CsvException {
+
+        String fileName = "C://Users//ekilabd//OneDrive - Ericsson//Documents//GitHub//Java-ReadnWritecsv//input-data.csv";
+        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build(); // custom separator
+        
+        //Creates an instance of CSVReader and reads all the data from the CSV file using the readAll() method
+        
+        try(CSVReader reader = new CSVReaderBuilder(
+                new FileReader(fileName))
+                .withCSVParser(csvParser)   // custom CSV parser
+                .withSkipLines(1)           // skip the first line, header info
+                .build()){
+        	
+        	//The data read from the file is stored in the r variable as a list of string arrays
+            r = reader.readAll();
+        }
+        
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
 }
